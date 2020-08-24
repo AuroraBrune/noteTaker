b850933158bfa668253d83ab1c218fb0c6975f5c
Aurora Brune <akbrune@hotmail.com>
2020-08-24 15:17:48 -0500

    initial setup of server.js and npm

---
+++ server.js
@@ -0,0 +1,23 @@
+const bodyParser = require('body-parser')
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = process.envPORT || 8080;
+
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
